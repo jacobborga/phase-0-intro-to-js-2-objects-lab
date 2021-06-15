7e44aaac7fa3be893b2c7ff07cb109726953c2eb
jacobborga <jdborga@gmail.com>
2021-06-15 23:15:13 +0000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = { name: "Jacob", streetAddress: '1234 Carnitas Rd.' };
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const copy = { ...employee };
+    copy[key] = value;
+    return copy;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const copy = { ...employee };
+    copy[key] = undefined;
+    return copy;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    employee[key] = undefined;
+    return employee;
+}
